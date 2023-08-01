0537959e48750e4705b3e61785c780c975f0b28a
jainprerit15 <81305293+jainprerit15@users.noreply.github.com>
2023-08-01 17:05:34 +0530

    This is my project of keeper app.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header.jsx";
+import Footer from "./footer.jsx";
+import Content from "./content.jsx";
+
+function App(){
+return (<div>
+    <Header/>
+    <Footer/> 
+    <Content/>
+</div>);
+
+}
+export default App;
---
+++ src/components/content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function content(){
+return (
+    <div classname="note">
+    <h1>This is keep notes </h1>
+    <p>This actually helps in taking notes.</p>
+    </div>
+);
+
+}
+export default content;
